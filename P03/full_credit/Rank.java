3351bbaee50e29f905c45c68cba3865d53865568
Andrew Nguyen <nguyen.andrew1234@yahoo.com>
2022-09-12 14:20:47 -0500

    Initial Commit P03

---
+++ P03/full_credit/Rank.java
@@ -0,0 +1,8 @@
+class Rank
+{
+    public static final int min = 0;
+    public static final int max = 9;
+    public int n;
+    
+    int inRange(int min, int max, int n)
+}
---
+++ P03/full_credit/Suit.java
@@ -0,0 +1,4 @@
+private enum Suit
+{
+    U, T, A;
+}
