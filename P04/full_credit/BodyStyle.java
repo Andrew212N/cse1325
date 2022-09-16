a9f406e72417d82f63d101334493304feb5f5d4c
Andrew Nguyen <nguyen.andrew1234@yahoo.com>
2022-09-15 23:13:04 -0500

    Inital commit P04

---
+++ P04/full_credit/BodyStyle.java
@@ -0,0 +1,9 @@
+public enum BodyStyle
+{
+    Sedan,
+    Hatchback,
+    Minivan,
+    Truck,
+    Crossover,
+    SUV;
+}
---
+++ P04/full_credit/Vehicle.java
@@ -0,0 +1,21 @@
+public class Vehicle
+{
+    private int year;
+    private String make;
+    private String model;
+    private BodyStyle bodystyle;
+
+    public Vehicle(int year, String make, String model, Bodystyle bodystyle)
+    {
+        this.year = year;
+        this.make = make;
+        this.model = model;
+        this.bodystyle = bodystyle;
+    }
+    
+    public double range()
+    public double fuelConsumed()
+    public dollarsToTravel()
+    
+    public String toString()
+}
