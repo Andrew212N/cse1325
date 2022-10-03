dcd0ecfa62ac2a9f9d3b5e5d78b32e7be9e506e6
Andrew Nguyen <nguyen.andrew1234@yahoo.com>
2022-10-03 00:10:19 -0500

    Inital commit P05

---
+++ P05/full_credit/Scoop.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class Scoop
+{
+    private IceCreamFlavor flavor;
+    private MixIn mixins[];
+    
+    public Scoop (IceCreamFlavor flavor)
+    {
+        this.flavor = flavor;
+        ArrayList<MixIn> toppings = new ArrayList<MixIn>();
+    }
+    
+    public addMixIn(MixIn mixins[])
+    {
+        
+    }
+    
+    public String toString()
+    {
+        return 
+    }
+    
+}
