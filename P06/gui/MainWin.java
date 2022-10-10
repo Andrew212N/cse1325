a377ebb9b768cf6d8689d78899b0b89961212515
Andrew Nguyen <nguyen.andrew1234@yahoo.com>
2022-10-10 14:18:35 -0500

    Inital P06 Commit, Screen final Commit

---
+++ P06/gui/MainWin.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class MainWin extends JFrame
+{
+    public MainWin(String title)
+    {
+        
+    }
+}
---
+++ P06/gui/Screen.java
@@ -0,0 +1,6 @@
+public enum Screen
+{
+    ICE_CREAM_FLAVORS,
+    MIX_IN_FLAVORS,
+    SCOOPS;
+}
