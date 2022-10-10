a60a6627a0499f13cb39b0ebb5118af9fbbde9bd
Andrew Nguyen <nguyen.andrew1234@yahoo.com>
2022-10-10 14:17:17 -0500

    inital commit P06, cited Rice in source code

---
+++ P06/gui/emporium/product/MixIn.java
@@ -0,0 +1,24 @@
+/***************************************************************************************
+*    Title: MixIn
+*    Author: George Rice
+*    Date: 10/4/2022
+*    Code version: 1.0
+*    Availability: https://github.com/prof-rice/cse1325-prof/tree/main/P05
+*
+***************************************************************************************/
+
+public class MixIn {
+    public MixIn(MixInFlavor flavor, MixInAmount amount) {
+        this.flavor = flavor;
+        this.amount = amount;
+    }
+    @Override
+    public String toString() {
+//        return flavor.toString() + ((amount != MixInAmount.Normal)
+        return flavor.toString() + (!amount.equals(MixInAmount.Normal)
+                                 ? " (" + amount + ")"
+                                 : "");
+    }
+    private MixInFlavor flavor;
+    private MixInAmount amount;
+}
