170890a25fa6ad21212526c2a3ccf3c518fbcb8c
Andrew Nguyen <nguyen.andrew1234@yahoo.com>
2022-10-10 14:17:55 -0500

    Inital Commit P06 Emporium

---
+++ P06/gui/emporium/Emporium.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class Emporium
+{
+    private ArrayList<MixInFlavor> mixins = new ArrayList<>();
+    private ArrayList<IceCreamFlavor> flavors = new ArrayList<>();
+    private ArrayList<Scoop> scoops = new ArrayList<>();
+}
