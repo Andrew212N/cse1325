2e341951dd2bbe2f8b358351ad82ade9c11ca7d1
Andrew Nguyen <nguyen.andrew1234@yahoo.com>
2022-10-10 19:12:34 -0500

    1st commit

--- P06/gui/emporium/Emporium.java
+++ P06/gui/emporium/Emporium.java
@@ -5,4 +5,32 @@ public class Emporium
     private ArrayList<MixInFlavor> mixins = new ArrayList<>();
     private ArrayList<IceCreamFlavor> flavors = new ArrayList<>();
     private ArrayList<Scoop> scoops = new ArrayList<>();
+    
+    public void addMixInFlavor(MixInFlavor flavor)
+    {
+        mixins.add(new MixInFlavor(flavor));
+    }
+    
+    public void addIceCreamFlavor(IceCreamFlavor flavor)
+    {
+        flavors.add(new IceCreamFlavor(flavor));
+    }
+    
+    public void addScoop(Scoop scoop)
+    {
+        scoops.add(new Scoop(scoop));
+    }
+    
+    public void mixInFlavors()
+    {
+        mixInFlavors.toArray();
+    }
+    public Object[] iceCreamFlavors()
+    {
+        iceCreamFlavors.toArray();
+    }
+    public Object[] scoops()
+    {
+        scoops.toArray();
+    }
 }
